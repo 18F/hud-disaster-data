912ab354dfadf3d0384a23bf2d9c3c68aa87ee3b
Jesse Piascik <jesse@piascik.net>
2017-04-26 15:09:08 -0600

    Serve hello world at root

---
+++ test/app.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('../app.js');
+
+describe('GET /', function() {
+  it('should return 200 OK', function(done) {
+    request(app)
+      .get('/')
+      .expect(200, done);
+  });
+});
