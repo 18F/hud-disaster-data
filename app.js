7ec81145e5c7060d4cd98fa1240a726e3f6e9ddd
Jesse Piascik <jesse@piascik.net>
2017-05-04 11:44:07 -0600

    Add pug and create login route in main controller

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const controllers = require('./lib/controllers');
+const mainController = controllers.main;
 
 app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
+app.use('/', mainController);
 
 module.exports = app;
--- test/test-app.js
+++ test/test-app.js
@@ -1,10 +1,18 @@
 const request = require('supertest');
 const app = require('../app.js');
 
-describe('GET /', function() {
-  it('should return 200 OK', function(done) {
+describe('GET /', () => {
+  it('should return 200 OK', (done) => {
     request(app)
       .get('/')
+      .expect(200, done);
+  });
+});
+
+describe.only('GET /login', () => {
+  it('should return 200 ok', (done) => {
+    request(app)
+      .get('/login')
       .expect(200, done);
   });
 });
