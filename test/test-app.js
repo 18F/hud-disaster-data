be1b953c10e863bbc0341196177b12c37fff4d06
Jesse Piascik <jesse@piascik.net>
2017-05-04 15:35:51 -0600

    Check for password and add success after login page

--- test/test-app.js
+++ test/test-app.js
@@ -34,14 +34,15 @@ describe('GET /login', () => {
   });
 });
 
-describe.only('POST /login', () => {
+describe('POST /login', () => {
   it('should return 403 Forbidden if _csrf is empty', (done) => {
     request(app).post('/login')
     .type('form')
     .send({username: 'test', password: 'test'})
     .expect(403, done);
   });
-  it.only('should return 401 Unauthorized if username is empty', (done) => {
+  
+  it('should return 401 Unauthorized if username is empty', (done) => {
     var agent = request.agent(app);
     agent.get('/login')
     .end((err, res) => {
@@ -51,6 +52,32 @@ describe.only('POST /login', () => {
       .type('form')
       .send({_csrf: csrf, password: 'test'})
       .expect(401, done);
+    });
+  });
+
+  it('should return 401 Unauthorized if password is empty', (done) => {
+    var agent = request.agent(app);
+    agent.get('/login')
+    .end((err, res) => {
+      const $ = cheerio.load(res.text);
+      const csrf = $('form input[name=_csrf]').val();
+      agent.post('/login')
+      .type('form')
+      .send({_csrf: csrf, username: 'test'})
+      .expect(401, done);
+    });
+  });
+
+  it('should return 200 ok if username and password are valid', (done) => {
+    var agent = request.agent(app);
+    agent.get('/login')
+    .end((err, res) => {
+      const $ = cheerio.load(res.text);
+      const csrf = $('form input[name=_csrf]').val();
+      agent.post('/login')
+      .type('form')
+      .send({_csrf: csrf, username: 'test', password: 'test'})
+      .expect(200, done);
     });
   });
 });
