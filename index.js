f61323d308f77ccceb0eb310bf8e6f99b759c561
Dave Hannon <dhannon@compcc.com>
2017-05-17 09:10:02 -0500

    added web.config for IIS/Azure, changed index.js to not set PORT if already set (by Azure)

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 'use strict';
 
-const app   = require('./app');
-const port  = process.env.PORT || 3000;
+const app = require('./app')
+const port = process.env.PORT ? process.env.PORT : process.env.PORT || 3000
 
 let server = app.listen(port, () => {
-  let address = server.address().address;
+  let address = server.address().address
 
-  if (address == '::') {
-    address = 'localhost';
+  if (address === '::') {
+    address = 'localhost'
   }
-  address += ':';
+  address += ':'
 
-  console.log('Running hud-disaster-data app at', address + port);
-});
+  console.log('Running hud-disaster-data app at', address + port)
+})
 
-module.exports = server;
+module.exports = server
