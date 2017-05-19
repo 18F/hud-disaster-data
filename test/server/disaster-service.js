9344c3ec6bd081643ec1a773fbcb9f4a938b0188
Jesse Piascik <jesse@piascik.net>
2017-05-19 09:05:05 -0600

    Increase timeout for server tests

--- test/server/disaster-service.js
+++ test/server/disaster-service.js
@@ -3,7 +3,9 @@ const app = require('../../app.js')
 const should = require('should')
 const moment = require('moment')
 
-describe('/api/disasters/:qry', () => {
+describe('/api/disasters/:qry', function () {
+  this.timeout(5000)
+  
   it('should return disasters with a disaster type matching the first two characters in the qry path parameter', (done) => {
     request(app).get('/api/disasters/FM')
     .expect(function(res) {
