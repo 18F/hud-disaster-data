06decacc750149c518d2974363f7e8245e852804
Bryan McFadden <bmcfadden@flexion.us>
2017-05-23 18:16:14 -0400

    Updated typeahead styling

--- src/components/Typeahead.vue
+++ src/components/Typeahead.vue
@@ -1,29 +1,30 @@
 <template>
-  <div class="Typeahead">
-    <div id="search">
-      <i class="fa fa-spinner fa-spin" v-if="loading"></i>
-      <template v-else>
-        <i class="fa fa-search" v-show="isEmpty"></i>
-        <i class="fa fa-times" v-show="isDirty" @click="reset"></i>
-      </template>
-      <input type="text"
-             class="Typeahead__input"
-             placeholder="Search disaster #"
-             autocomplete="off"
-             v-model="query"
-             @keydown.down="down"
-             @keydown.up="up"
-             @keydown.enter="hit"
-             @keydown.esc="reset"
-             @input="update"/>
-
-      <ul v-show="hasItems" class="disaster-list">
-        <li v-for="(item, $item) in items" :class="activeClass($item)" @mousemove="setActive($item)">
-          <disaster v-bind:item="item"></disaster>
-        </li>
-      </ul>
-    </div>
-    <savedExtracts></savedExtracts>
+  <div class="wrapper">
+    <div>
+      <div class="Typeahead">
+          <i class="fa fa-spinner fa-spin" v-if="loading"></i>
+          <template v-else>
+            <i class="fa fa-search" v-show="isEmpty"></i>
+            <i class="fa fa-times" v-show="isDirty" @click="reset"></i>
+          </template>
+          <input type="text"
+                 class="Typeahead__input"
+                 placeholder="Search disaster #"
+                 autocomplete="off"
+                 v-model="query"
+                 @keydown.down="down"
+                 @keydown.up="up"
+                 @keydown.enter="hit"
+                 @keydown.esc="reset"
+                 @input="update"/>
+          <ul id="disaster-list" v-show="hasItems" class="disaster-list">
+            <li v-for="(item, $item) in items" :class="activeClass($item)" @mousemove="setActive($item)">
+              <disaster v-bind:item="item"></disaster>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <savedExtracts></savedExtracts>
   </div>
 </template>
 
@@ -54,38 +55,28 @@ export default {
 
 
 <style src="../../public/assets/css/font-awesome.min.css"/>
-
 <style scoped>
-.Typeahead {
-  position: relative;
+.wrapper div:first-child {
+  width:50%;
+  margin:0 auto;
 }
-
+.Typeahead {  position: relative; padding:10px 0;}
 .Typeahead__input {
-  width: 100%;
+  margin:0 auto;
   font-size: 14px;
   color: #2c3e50;
   line-height: 1.42857143;
-  box-shadow: inset 0 1px 4px rgba(0,0,0,.4);
-  -webkit-transition: border-color ease-in-out .15s,-webkit-box-shadow ease-in-out .15s;
-  transition: border-color ease-in-out .15s,box-shadow ease-in-out .15s;
   font-weight: 300;
-  padding: 12px 26px;
-  border: none;
-  border-radius: 22px;
+  max-width:100%;
+  border: 1px solid #ccc;
   letter-spacing: 1px;
   box-sizing: border-box;
 }
-
 .Typeahead__input:focus {
-  border-color: #4fc08d;
   outline: 0;
-  box-shadow: inset 0 1px 1px rgba(0,0,0,.075),0 0 8px #4fc08d;
 }
 
-.fa-times {
-  cursor: pointer;
-}
-
+.fa-times { cursor: pointer; }
 i {
   float: right;
   position: relative;
@@ -94,44 +85,35 @@ i {
   opacity: 0.4;
 }
 
-ul {
+#disaster-list {
   position: absolute;
+  top:35px;
+  border-right:1px solid #ccc;
+  border-left:1px solid #ccc;
   padding: 0;
-  margin-top: 8px;
   min-width: 100%;
   background-color: #fff;
-  list-style: none;
-  border-radius: 4px;
-  box-shadow: 0 0 10px rgba(0,0,0, 0.25);
+  list-style-type: none;
   z-index: 1000;
 }
-
-li {
-  padding: 10px 16px;
+#disaster-list li:before { content: ''; }
+#disaster-list li {
+  display:block;
+  padding: 10px;
   border-bottom: 1px solid #ccc;
+  margin:0;
+  line-height:20px;
   cursor: pointer;
 }
-
-li:first-child {
-  border-top-left-radius: 4px;
-  border-top-right-radius: 4px;
-}
-
-li:last-child {
-  border-bottom-left-radius: 4px;
-  border-bottom-right-radius: 4px;
-  border-bottom: 0;
-}
+#disaster-list li:first-child {}
+#disaster-list li:last-child {}
 
 span {
   display: block;
   color: #2c3e50;
 }
 
-.active {
-  background-color: #3aa373;
-}
-
+.active { background-color: #f1f1f1; }
 .active span {
   color: white;
 }
@@ -140,42 +122,5 @@ span {
   font-weight: 700;
   font-size: 18px;
 }
-
-.screen-name {
-  font-style: italic;
-}
-.disaster {
-     font-family:sans-serif;
-     padding:10px;
- }
- .disaster a, .disaster a:hover, .disaster a:active {
-   text-decoration:none;
- }
- .disaster .name {
-     font-weight:bold;
-     padding-bottom:20px;
- }
- .disaster .name div:first-child {
-   float:left;
-   padding-right:15px;
- }
- .disaster .declaration div { float:right; }
- .disaster .declaration div:first-child {
-   float:left;
- }
- .disaster .declaration i { margin-left:5px; }
- .disaster .counties {
-     clear:both;
-     display:block;
-     padding:10px 0;
- }
- .disaster .counties div {
-   background:#f7f7f7;
-   border:1px solid #eee;
- }
- .disaster .counties ul {
-     list-style-type:none;
-     margin:0;
-     padding:10px;
- }
+.screen-name { font-style: italic; }
 </style>
