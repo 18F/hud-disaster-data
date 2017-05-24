373e28f4c89d49ff58029cc9d17ec809c4b33d34
cortney robinson <gvestudios@gmail.com>
2017-05-24 13:41:54 -0400

    hud header comp update

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,21 +1,5 @@
 <template>
   <div id="app">
-    <div class="header --global">
-      <div class="header header--pre">
-        <div class="container-block">
-          <p>An official website of the United States Government</p>
-        </div>
-      </div>
-      <div class="header masthead">
-        <div class="container-block">
-          <div class="logo logo--block">
-            <span class="logo-img"></span>
-            <p>Disaster Data Portal</p>
-          </div>
-        </div>
-      </div>
-    </div>
-
     <router-view></router-view>
   </div>
 </template>
---
+++ src/components/Header/index.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  name: 'header',
+  data () {
+    return {
+      title: 'Disaster Data Portal',
+      premsg: 'An official website of the United States Government'
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="header --global" id="appHeader">
+    <div class="header header--pre">
+      <div class="container-block">
+      <p><i class="icon icon--flag"></i>{{premsg}}</p>
+      </div>
+    </div>
+    <div class="header header--masthead">
+      <div class="container-block">
+        <div class="logo logo--block">
+          <p class="logo-img"></p>
+          <p>{{title}}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
