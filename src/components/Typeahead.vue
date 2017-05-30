22ee43822d75326d5e2904f48b7aef439a9961e3
Bryan McFadden <bmcfadden@flexion.us>
2017-05-30 11:51:52 -0400

    Added styling to the extract list side

--- src/components/Typeahead.vue
+++ src/components/Typeahead.vue
@@ -39,9 +39,27 @@
             </div>
         </div>
       </div>
-      <div class="col-xs-12 col-md-6" style="border:1px solid #00ff00; background:#000; overflow:auto; border-radius:20px;">
-        <savedExtracts ref="extracts" v-on:unselected="onUnSelected"></savedExtracts>
-      </div>
+      <div class="col-xs-12 col-md-6 extracts">
+          <div id="saved_searches">
+              <select>
+                <option value="">My Saved Searches</option>
+                <option value="">Saved Search 1</option>
+              </select>
+              <button class="usa-button">
+                <i class="fa fa-2x fa-save"></i>
+              </button>
+              <button class="usa-button">
+                <i class="fa fa-2x fa-trash-o"></i>
+              </button>
+          </div>
+          <div id="list">
+            <savedExtracts ref="extracts" v-on:unselected="onUnSelected"></savedExtracts>
+          </div>
+          <div id="action-buttons">
+            <button disabled="true" class="usa-button usa-button-disabled">Export</button>
+            <button disabled="true" class="usa-button usa-button-disabled">Clear</button>
+          </div>
+        </div>
     </div>
   </div>
 </template>
@@ -100,7 +118,7 @@ export default {
   height:50px;
 }
 .search-container {
-  margin:150px 0;
+  margin:200px 0;
   padding:0;
 }
 #search .fa-times { cursor:pointer; opacity:0.4;}
@@ -145,13 +163,13 @@ export default {
   top:135px;
   z-index: 1000;
 }
-.disaster-list ul {
+.disaster-list ul, #extracts {
   display:block;
   margin:0;
   list-style-type: none;
 }
-.disaster-list li:before { content: ''; }
-.disaster-list li {
+.disaster-list li:before, #extracts li:before { content: ''; }
+.disaster-list li, #extracts li {
   display:block;
   border-bottom: 1px solid #ccc;
   margin:0;
@@ -159,6 +177,50 @@ export default {
   cursor: pointer;
 }
 
+/* Extract list styles --------------------------------- */
+.extracts {
+  background: url('/static/img/bg_80_opacity.png');
+  overflow:hidden;
+  border-radius:20px;
+  padding:30px 20px;
+}
+.extracts div#saved_searches {
+  height:70px;
+}
+.extracts div#saved_searches select{
+  float:left;
+  margin-right:20px;
+  width:77%;
+}
+.extracts div#saved_searches button{
+  background:transparent;
+  border-radius:4px;
+  margin:0;
+  padding:10px;
+}
+.extracts div#saved_searches button:focus {
+  box-shadow:none;
+}
+.extracts div#saved_searches i {
+  cursor:pointer;
+  float:left;
+}
+.extracts div#list {
+  background: url('/static/img/bg_80_opacity.png');
+  border:1px solid #000;
+  clear:both;
+  overflow-y:scroll;
+  overflow:auto;
+  height:410px;
+}
+.extracts div#action-buttons {
+  height:50px;
+  margin-top:20px;
+}
+.extracts div#action-buttons button {
+  margin-left:20px;
+  float:right;
+}
 span {
   display: block;
   color: #2c3e50;
