6a2551dbb0d49f21ecf439f889ce61bce0f2b4ce
Jesse <jesse@piascik.net>
2017-06-01 13:51:25 -0600

    Update css for dropdown

--- src/components/SavedExtracts.vue
+++ src/components/SavedExtracts.vue
@@ -167,9 +167,13 @@ export default {
     max-height:44px;
   }
   select {
-    -webkit-appearance: menulist;
-    -moz-appearance: menulist;
-    appearance: menulist;
+    -webkit-appearance: none;
+    -webkit-border-radius: 0px;
+    background: url("data:image/svg+xml;utf8,<svg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='24' height='24' viewBox='0 0 24 24'><path fill='#000' d='M7.406 7.828l4.594 4.594 4.594-4.594 1.406 1.406-6 6-6-6z'></path></svg>");
+    background-position: 100% 50%;
+    background-repeat: no-repeat;
+    background-color: white;
+    height: 44px;
   }
 }
 .extracts div#saved_searches button{
