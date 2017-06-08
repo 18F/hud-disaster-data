1299f39c06cfde5b08378b586357da8165eca829
Jesse <jesse@piascik.net>
2017-06-08 12:30:46 -0600

    Add shepherd button css

--- src/tour.js
+++ src/tour.js
@@ -2,22 +2,43 @@ import Shepherd from 'tether-shepherd'
 
 const tour = new Shepherd.Tour({
   defaults: {
-    classes: 'shepherd-element shepherd-open shepherd-theme-arrows',
+    classes: 'shepherd-element shepherd-open shepherd-theme-square',
     showCancelLink: true,
     scrollTo: true
   }
 })
 
+let backNext = [
+  {
+    text: 'Back',
+    action: tour.back
+  },
+  {
+    text: 'Next',
+    action: tour.next
+  }
+]
+
 tour.addStep('enter-search', {
   text: 'Start here by typing in a FEMA disaster ID in the format "DR-4272-TX"',
   attachTo: '.search-wrapper bottom',
-  classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
+  advanceOn: '.disaster-search-recs DOMSubtreeModified',
   buttons: [
     {
       text: 'Next',
       action: tour.next
     }
   ]
+})
+.addStep('select-disasters', {
+  text: 'Click the box to select a disaster',
+  attachTo: '.disaster-list top',
+  buttons: backNext
+})
+.addStep('save-search', {
+  text: 'You\'ve selected a disaster and it is now listed here. You can either export the associated FEMA data, or save this search to export the data in the future.',
+  attachTo: '#saved_searches',
+  buttons: backNext
 })
 
 export default {
