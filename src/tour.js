772c144332b9b34e47d30642cfce3a66a0f8ff3a
Jesse <jesse@piascik.net>
2017-06-08 11:39:29 -0600

    css for tour backdrop

--- src/tour.js
+++ src/tour.js
@@ -2,14 +2,16 @@ import Shepherd from 'tether-shepherd'
 
 const tour = new Shepherd.Tour({
   defaults: {
-    classes: 'shepherd-theme-arrows',
+    classes: 'shepherd-element shepherd-open shepherd-theme-arrows',
+    showCancelLink: true,
     scrollTo: true
   }
 })
 
-tour.addStep('example-step', {
-  text: 'Check out this cool button!',
-  attachTo: '.avi bottom',
+tour.addStep('enter-search', {
+  text: 'Start here by typing in a FEMA disaster ID in the format "DR-4272-TX"',
+  attachTo: '.search-wrapper bottom',
+  classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
   buttons: [
     {
       text: 'Next',
