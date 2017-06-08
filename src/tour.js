e2074dd3185790b6f3ae3f875d1e89e0962e1262
Jesse <jesse@piascik.net>
2017-06-08 15:17:11 -0600

    Quick edits on tour

--- src/tour.js
+++ src/tour.js
@@ -4,7 +4,7 @@ const tour = new Shepherd.Tour({
   defaults: {
     classes: 'shepherd-element shepherd-open shepherd-theme-square',
     showCancelLink: true,
-    scrollTo: true
+    scrollTo: false
   }
 })
 
@@ -16,25 +16,23 @@ let next = {
   text: 'Next',
   action: tour.next
 }
-let cancel = {
-  text: 'Cancel',
-  action: tour.cancel
-}
+// let cancel = {
+//   text: 'Cancel',
+//   action: tour.cancel
+// }
 
 tour.addStep('enter-search', {
   text: 'Start here by typing in a FEMA disaster ID in the format "DR-4272-TX"',
-  attachTo: '.search-wrapper bottom',
-  advanceOn: '.disaster-search-recs DOMSubtreeModified',
-  buttons: [cancel]
+  attachTo: '.search-wrapper right'
 })
 .addStep('select-disasters', {
   text: 'Click the box to select a disaster',
-  attachTo: '.disaster-list top',
+  attachTo: '.disaster-list right',
   buttons: [back, next]
 })
 .addStep('save-search', {
   text: 'You\'ve selected a disaster and it is now listed here. You can either export the associated FEMA data, or save this search to export the data in the future.',
-  attachTo: '#saved_searches top',
+  attachTo: '.extracts left',
   buttons: [back, next]
 })
 
