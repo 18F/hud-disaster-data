940f705d7f40cacb0049fd51a7feac41d63300ff
Jesse <jesse@piascik.net>
2017-06-19 14:43:53 -0600

    Add mixin to tests

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import './vue-mixins'
 import Vuex from 'vuex'
 import Axios from 'axios'
 import store from './store'
@@ -8,25 +9,8 @@ import Sprites from './components/Sprites'
 import Typeahead from './components/Typeahead'
 import AppHeader from './components/Header'
 import AppFooter from './components/Footer'
-import icon from './components/Icon'
-
 import es6Promise from 'es6-promise'
 es6Promise.polyfill()
-
-const messages = {
-  success: 'fa-check-circle',
-  error: 'fa-times-circle',
-  warning: 'fa-warning',
-  info: 'fa-info-circle'
-}
-Vue.mixin({
-  components: {icon},
-  methods: {
-    iconName () {
-      return messages[this.status.type]
-    }
-  }
-})
 
 Vue.use(Vuex)
 Vue.config.productionTip = false
