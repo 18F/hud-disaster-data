14dc67ae221241d7425bc8de0379fed64c1eb8b1
Chris Barrett <cbarrett@flexion.us>
2017-07-14 22:33:03 +0200

    first working prototype of vue-select

--- src/components/Report.vue
+++ src/components/Report.vue
@@ -1,14 +1,18 @@
 <template lang="pug">
 div(style='color: #fff; font-size: xx-large;', tabindex='0')
   | The purpose of this page is to produce a standard report that summarizes the extent of disaster damages for a particular location (for example: state, county, city, or congressional district).
+  div
+    selectLocation()
 </template>
 
 <script>
+import selectLocation from './SelectLocation'
 /**
 * Component responsible for displaying a report or reports.  This is just a stub, at this time.
 * @module components/Report
 */
 export default {
-  name: 'report'
+  name: 'report',
+  components: {selectLocation}
 }
 </script>
