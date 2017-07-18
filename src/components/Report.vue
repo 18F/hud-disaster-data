1477e5526e448fc48963d9e9afd1b59935c53472
Chris Barrett <cbarrett@flexion.us>
2017-07-18 21:13:14 +0200

    Styling changes, quick commit to get latest in

--- src/components/Report.vue
+++ src/components/Report.vue
@@ -2,17 +2,17 @@
 div(style='color: #fff; font-size: xx-large;', tabindex='0')
   | The purpose of this page is to produce a standard report that summarizes the extent of disaster damages for a particular location (for example: state, county, city, or congressional district).
   div
-    selectLocation()
+    selectLocationSideBar()
 </template>
 
 <script>
-import selectLocation from './SelectLocation'
+import selectLocationSideBar from './SelectLocationSideBar'
 /**
 * Component responsible for displaying a report or reports.  This is just a stub, at this time.
 * @module components/Report
 */
 export default {
   name: 'report',
-  components: {selectLocation}
+  components: {selectLocationSideBar}
 }
 </script>
