a29c50a55db6d981514f0f36e1ce906b88d8ffaf
Dave Hannon <dhannon@compcc.com>
2017-07-19 07:44:54 -0500

    fixed tests after they broke due to modulization of store

--- test/unit/specs/DisasterSearch.spec.js
+++ test/unit/specs/DisasterSearch.spec.js
@@ -3,7 +3,8 @@ import Vue from 'vue' // eslint-disable-line
 import '@/vue-mixins'
 import Vuex from 'vuex' // eslint-disable-line
 import sinon from 'sinon'
-import store, {actions, getters, mutations} from '../../../src/searchStore' // eslint-disable-line
+import store from '../../../src/store' // eslint-disable-line
+import {actions, getters, mutations} from '../../../src/searchStore' // eslint-disable-line
 import DisasterSearch from '@/components/DisasterSearch' // eslint-disable-line
 import Message from '@/components/Message' // eslint-disable-line
 import magic from '@/bus'
@@ -30,6 +31,7 @@ describe('DisasterSearch.vue', () => {
   it('should render correct list item contents', done => {
     const Constructor = Vue.extend(DisasterSearch)
     const vm = new Constructor({store}).$mount()
+    debugger
     vm.$store.commit('updateDisasterList', ONE_RECORD)
     Vue.nextTick(function () {
       expect(vm.$el.querySelector('.disaster-search-recs').childElementCount).to.be.equal(1)
