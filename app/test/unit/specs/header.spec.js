d32b579542f60f13b4e68b1167c813d76fbb9116
Dave Hannon <dhannon@compcc.com>
2017-08-03 14:30:11 -0500

    fixed test for Header

--- app/test/unit/specs/header.spec.js
+++ app/test/unit/specs/header.spec.js
@@ -6,6 +6,7 @@ import tour from '@/tour'
 import sinon from 'sinon'
 import header from '@/components/Header'
 import router from '@/router'
+import store from '@/store'
 // eslint-disable-line
 
 Vue.config.productionTip = false
@@ -13,7 +14,7 @@ Vue.config.productionTip = false
 describe('Header.vue', () => {
   it('should render correct header', done => {
     const Constructor = Vue.extend(header)
-    const vm = new Constructor({router}).$mount()
+    const vm = new Constructor({store, router}).$mount()
     Vue.nextTick(function () {
       expect(vm.$refs.title.textContent).to.be.equal(vm.title)
       done()
@@ -23,7 +24,7 @@ describe('Header.vue', () => {
     it('should start the tour', () => {
       let startStub = sinon.stub(tour, 'start')
       const Constructor = Vue.extend(header)
-      const vm = new Constructor({router}).$mount()
+      const vm = new Constructor({store, router}).$mount()
       vm.startTour()
       expect(startStub.called).to.be.equal(true)
       startStub.restore()
@@ -32,7 +33,7 @@ describe('Header.vue', () => {
   describe('toggleBurger', () => {
     it('should set burgerMenu attribute hidden if it is not set, or unset it, if it is set', () => {
       const Constructor = Vue.extend(header)
-      const vm = new Constructor({router}).$mount()
+      const vm = new Constructor({store, router}).$mount()
       expect(vm.$el.querySelector('#burger-menu.hidden')).to.not.be.null
       vm.toggleBurger()
       expect(vm.$el.querySelector('#burger-menu.hidden')).to.be.null
@@ -41,7 +42,7 @@ describe('Header.vue', () => {
   describe('skipToContent', () => {
     it('should set focus on Guide Me button', () => {
       const Constructor = Vue.extend(header)
-      const vm = new Constructor({router}).$mount()
+      const vm = new Constructor({store, router}).$mount()
       expect(document.activeElement.nodeName).to.be.equal('BODY')
       const focusStub = sinon.stub(vm.$refs.guideMe, 'focus')
       vm.skipToContent()
