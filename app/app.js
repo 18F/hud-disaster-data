896ca9d743a063a937089f65a3c4d61a48b125c5
Jesse <jesse@piascik.net>
2017-09-07 16:58:49 -0600

    Log all headers

--- app/app.js
+++ app/app.js
@@ -7,37 +7,22 @@ const bodyParser = require('body-parser')
 const lusca = require('lusca')
 const cookieSession = require('cookie-session')
 const morgan = require('morgan')
-
 const apiController = require('./lib/controllers/api')
-require('./lib/swagger')(app)
-// const controllers = require('./lib/controllers');
-// const mainController = controllers.main;
-app.use(morgan(/dev/.test(process.env.NODE_ENV) ? 'dev' : 'combined'))
 const dayInMillis = 24 * 60 * 60 * 1000
+require('./lib/swagger')(app)
+
+app.use(morgan(/dev/.test(process.env.NODE_ENV) ? 'dev' : 'combined'))
+app.use(function (req, res, next) {
+  console.log(req.headers)
+  next()
+})
 app.use(cookieSession({
   secret: process.env.COOKIE_SECRET || 'secret!@#',
   maxAge: dayInMillis
 }))
-
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-
-// app.use(lusca({
-//   csrf: true,
-//   xssProtection: true
-// }));
 app.use('/api', apiController)
-
-// handle fallback for HTML5 history API
-// app.use(require('connect-history-api-fallback')())
-
-const staticDir = path.join(__dirname, 'dist')
-
-app.use(express.static(staticDir))
-// app.use(flash())
-// app.set('view engine', 'pug');
-// app.set('views', __dirname + '/views');
-
-// require('./lib/services/authenticate').init(app);
+app.use(express.static(path.join(__dirname, 'dist')))
 
 module.exports = app
