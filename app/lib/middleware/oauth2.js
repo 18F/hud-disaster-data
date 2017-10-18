0725eb03e4ffdf142a1a87ce991e6d46d88ee4e6
Dave Hannon <dhannon@compcc.com>
2017-10-18 09:55:56 -0500

    [ci skip] more oauth tinkering for development testing

--- app/lib/middleware/oauth2.js
+++ app/lib/middleware/oauth2.js
@@ -5,7 +5,8 @@ const cookieUrl = `${process.env.HUD_OPENAM_BASE_URL}/json/authenticate?realm=/`
 const tokenUrl = `${process.env.HUD_OPENAM_BASE_URL}/oauth2/authorize?realm=/`
 const oamUserId = process.env.oamUserId
 const oamPassword = process.env.oamPassword
-const oAuth2Scope = process.env.oAuth2Scope
+const DRDPOAuth2Scope = process.env.DRDPOAuth2Scope
+const DRGROAuth2Scope = process.env.DRGROAuth2Scope
 const oAuth2ClientId = process.env.oAuth2ClientId
 const redirectUri = 'http://localhost:8000'
 
@@ -24,8 +25,12 @@ let cookieReqOptions = {
 
 module.exports = {
   getOAuth2TokenParam: function (scope) {
-    let message = `&response_type=id_token%20token&save_consent=1&decision=Allow&scope=openid%20profile%20${oAuth2Scope}&nonce=1234&client_id=${oAuth2ClientId}&redirect_uri=${redirectUri}`
     return new Promise((resolve, reject) => {
+      let chosenScope
+      if (scope === 'DRGR') chosenScope = DRGROAuth2Scope
+      else if (scope === 'DRDP') chosenScope = DRDPOAuth2Scope
+      else reject(new Error(`${scope} is not valid.  Should be DRGR or DRDP`))
+      let message = `&response_type=id_token%20token&save_consent=1&decision=Allow&scope=openid%20profile%20${chosenScope}&nonce=1234&client_id=${oAuth2ClientId}&redirect_uri=${redirectUri}`
       request(cookieReqOptions).then(results => {
         console.log(`got results.tokenId: >> ${results.tokenId} <<\n\n\n`)
         console.log(`got results: >> ${JSON.stringify(results)} <<\n\n\n`)
